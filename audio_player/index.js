7b7ec42ce8b470f5118c81c973845beb4e74c1c0
Anastasiya Tomasheva <siha.sole@gmail.com>
2024-09-20 21:00:36 +0300

    feat: add and style volume block

--- audio_player/index.js
+++ audio_player/index.js
@@ -15,12 +15,41 @@ fetch(path)
     let length = document.querySelector('.length');
     let current = document.querySelector('.current');
     let containerProgress = document.querySelector('#container-progress');
+    let volIcon = document.querySelector('#vol-icon');
+    let volumeContainer = document.querySelector ('.volume-container');
+    let volume = document.querySelector('.volume');
+    let noMusic = document.querySelector('#no-music');
 
     let numOfTrack = 0;
 
     audio.addEventListener ('loadeddata', () => {
         length.textContent = getTime(audio.duration);
         audio.volume = .5;
+    })
+console.log(volumeContainer)
+    volIcon.addEventListener("click", () => {
+        // console.log(volumeContainer)
+        if (volumeContainer.style.display == "none") {
+            volumeContainer.style.display = 'block';
+            volume.style.display = 'block';
+            noMusic.style.display = 'block';
+        } else {
+            volumeContainer.style.display = 'none';
+            volume.style.display = 'none';
+            noMusic.style.display = 'none';
+        }
+        
+    })
+
+    volumeContainer.addEventListener('click', event => {
+        let currentVolume = event.offsetX / event.target.offsetWidth
+        audio.volume = currentVolume;
+        volume.style.width = currentVolume * 100 + '%';
+    })
+
+    noMusic.addEventListener("click", () => {
+        audio.muted = !audio.muted;
+        noMusic.classList.toggle('muted');
     })
 
     containerProgress.addEventListener("click", event => {
@@ -32,12 +61,12 @@ fetch(path)
     setInterval(() => {
         let progress = document.querySelector("#progress");
         progress.style.width = audio.currentTime / audio.duration * 100 + "%";
-        if(audio.currentTime / audio.duration * 100 < 10 || audio.currentTime / audio.duration * 100 > 90){
-            progress.style.borderRadius = 50 + "%";
-        } else {
-            progress.style.borderTopRightRadius = 0;
-            progress.style.borderBottomRightRadius = 0;
-        }
+        // if(audio.currentTime / audio.duration * 100 < 10 || audio.currentTime / audio.duration * 100 > 90){
+        //     progress.style.borderRadius = 50 + "%";
+        // } else {
+        //     progress.style.borderTopRightRadius = 0;
+        //     progress.style.borderBottomRightRadius = 0;
+        // }
         current.textContent = getTime(audio.currentTime)
     },500)
 
