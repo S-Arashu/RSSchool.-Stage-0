0fde3d81b71d6d535588a484e78c8b631af361be
Anastasiya Tomasheva <siha.sole@gmail.com>
2024-09-20 11:53:47 +0300

    feat: add possibility playing track and change compositions

--- audio_player/index.js
+++ audio_player/index.js
@@ -6,4 +6,75 @@ fetch(path)
 .then(response => response.json())
 .then(music_data => {
 
+    let isPlay = false;
+    let audio = document.querySelector('audio');
+    let prev = document.querySelector('.prev');
+    let next = document.querySelector('.next');
+    let playBtn = document.querySelector('.toggle-music');
+    let img = document.querySelector('.toggle-music img')
+    let length = document.querySelector('.length');
+    let current = document.querySelector('.current');
+    let timePlay = 0;
+
+    let numOfTrack = 0;
+
+    audio.addEventListener ('loadeddata', () => {
+        length.textContent = getTime(audio.duration);
+        audio.volume = .7;
+    })
+
+    setInterval(() => {
+        current.textContent = getTime(audio.currentTime)
+    },500)
+
+    audio.src = music_data[numOfTrack].song;
+
+    function play(){
+        
+        if(!isPlay){
+            audio.play();
+            isPlay = true;
+            img.setAttribute('src', './img/pause.png');
+            img.style.margin = 0;
+        } else if (isPlay) {
+            audio.pause();
+            isPlay = false;
+            img.setAttribute('src', './img/play.png');
+            img.style.marginLeft = 7 + "%";
+        }
+        
+    }
+
+    playBtn.addEventListener('click', play);
+    next.addEventListener('click', () => {
+        isPlay = false;
+        numOfTrack += 1;
+        audio.src = music_data[numOfTrack].song;
+        if(numOfTrack > 14){
+            numOfTrack = 0;
+        }
+        play();
+    });
+
+    prev.addEventListener('click', () => {
+        isPlay = false;
+        numOfTrack -= 1;
+        audio.src = music_data[numOfTrack].song;
+        if(numOfTrack < 0){
+            numOfTrack = 14;
+        }
+        play();
+    });
+
+    
+
+    function getTime(time){
+
+        let sec = parseInt(time);
+        let min = parseInt(sec / 60);
+        sec -= min * 60;
+
+        return `${min} : ${String(sec).padStart(2,0)}`
+    }
+
 });
