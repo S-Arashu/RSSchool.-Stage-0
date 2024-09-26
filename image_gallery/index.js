3d0d27763bac93b91120dc0bdbba743ce8ae335f
Anastasiya Tomasheva <siha.sole@gmail.com>
2024-09-26 22:49:45 +0300

    feat: add search

--- image_gallery/index.js
+++ image_gallery/index.js
@@ -1,6 +1,9 @@
 "use strict";
 
-let url = `https://api.unsplash.com/photos/random?orientation=landscape&client_id=4arZoJEL1KuBR9gU7Mtfno9am5Ct7rzvjET3Jc3l9lQ&count=10`;
+let searchField = document.querySelector('#search');
+// searchField.addEventListener('onsubmit', () => { return false});
+let url = `https://api.unsplash.com/photos/random?orientation=landscape&client_id=4arZoJEL1KuBR9gU7Mtfno9am5Ct7rzvjET3Jc3l9lQ&count=20&query=${searchField.value}`;
+
 
 async function getData() {
     const res = await fetch(url);
@@ -14,9 +17,13 @@ async function getData() {
 }
 getData();
 
+let main = document.querySelector(".container");
+
 function showData(data) {
-    let main = document.querySelector(".container");
-    // let newData = 
+
+
+    
+    
 
     data.map((data) => {
         const img = document.createElement("img");
@@ -28,3 +35,28 @@ function showData(data) {
 
     
 }
+
+    let searchBtn = document.querySelector('button');
+
+    function search () {
+        if (searchField.value != '') {
+            url = `https://api.unsplash.com/photos/random?orientation=landscape&client_id=4arZoJEL1KuBR9gU7Mtfno9am5Ct7rzvjET3Jc3l9lQ&count=20&query=${searchField.value}`;
+            main.innerHTML = '';
+            // console.log(searchField.value)
+            // searchField.value = searchField.value;
+            getData();
+        } else {
+            alert("Please, enter the word");
+        }
+    }
+
+    searchBtn.addEventListener("click", search)
+    document.addEventListener("keyup", event => {
+        if (event.code === 'Enter'){
+            search();
+        }
+    })
+
+    searchField.onfocus = function(){
+        console.log(+searchField.value)
+    }
