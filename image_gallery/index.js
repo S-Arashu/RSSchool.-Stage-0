d2f5e60bcd638d5c454366547bcfe5a8f9b0b4bb
Anastasiya Tomasheva <siha.sole@gmail.com>
2024-09-27 17:58:08 +0300

    feat: add popup

--- image_gallery/index.js
+++ image_gallery/index.js
@@ -18,16 +18,47 @@ async function getData() {
 getData();
 
 let main = document.querySelector(".container");
+let body = document.querySelector('body')
 
 function showData(data) {
 
 
-    
+    document.addEventListener("click", (e) => {
+        let target = e.target;
+        console.log(target.className)
+        for(let i=0; i<data.length; i++){
+            console.log(data[i].id)
+            if(target.id == data[i].id){
+            let bg = document.createElement('div');
+            bg.classList.add('dark-block');
+            body.append(bg);
+            let item = document.createElement('div');
+            item.classList.add('item-block');
+            item.innerHTML = `
+            <img src='${data[i].urls.regular}'></img>
+            `
+            body.append(item);
+
+            document.body.style.overflow = 'hidden';
+        }
+        }
+        
+        if(target.className == 'dark-block'){
+            let itemBlock = document.querySelector('.item-block');
+            let darkBlock = document.querySelector('.dark-block');
+            document.body.style.overflow = '';
+            itemBlock.remove();
+            darkBlock.remove();
+        }
+    })
+
+
     
 
     data.map((data) => {
         const img = document.createElement("img");
         img.classList.add("gallery-img");
+        img.setAttribute('id', data.id)
         img.src = data.urls.small;
         img.alt = data.alt_description;
         main.append(img); 
