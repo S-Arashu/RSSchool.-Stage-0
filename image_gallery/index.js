f581440f0c4b20f51b65f6b6a25c23095cdfe341
Anastasiya Tomasheva <siha.sole@gmail.com>
2024-09-29 23:29:51 +0300

    feat: add text for empty page and clear code

--- image_gallery/index.js
+++ image_gallery/index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 let searchField = document.querySelector('#search');
-// searchField.addEventListener('onsubmit', () => { return false});
 let url = `https://api.unsplash.com/photos/random?orientation=landscape&client_id=4arZoJEL1KuBR9gU7Mtfno9am5Ct7rzvjET3Jc3l9lQ&count=20&query=${searchField.value}`;
 let likedPhoto = new Set();
 let favImg = document.querySelector('.fav-img');
 let container = document.querySelector('.container');
 let containerForFavImg = document.querySelector('.container-for-fav-img');
+let empty = document.querySelector('.empty');
 
 async function getData() {
     const res = await fetch(url);
@@ -25,14 +25,13 @@ getData();
 let main = document.querySelector(".container");
 let body = document.querySelector('body')
 
-// let isLike = false;
+let isLike = false;
 let imgSrc = './img/heart.png';
 let numOfImg;
 
 function showData(data) {
 
         data.map((data) => {
-        // main.innerHTML = '';
         const img = document.createElement("img");
         img.classList.add("gallery-img");
         img.setAttribute('id', data.id)
@@ -51,370 +50,114 @@ function showData(data) {
             } else {
                 imgSrc = './img/heart.png';
             }
-        // console.log(target.className)
         for(let i=0; i<data.length; i++){
-            // console.log(data[i].id)
-            
 
             if(target.id == data[i].id){
                 let numForLikes = data[i].likes;
-                if(likedPhoto.has(target.id)){
-                numForLikes = numForLikes + 1;
-            } 
-            let bg = document.createElement('div');
-            bg.classList.add('dark-block');
-            body.append(bg);
-            let item = document.createElement('div');
-            item.classList.add('item-block');
-            item.innerHTML = `
-            <img src='${data[i].urls.regular}' class="item-block-img" data-num="${data[i].id}"></img>
-            <div class="like-container">
-                <img src='${imgSrc}' class="like"></img>
-                <p class="like-count">${numForLikes}</p>
-            </div>
-            `
+                    if(likedPhoto.has(target.id)){
+                        numForLikes = numForLikes + 1;
+                    } 
+                let bg = document.createElement('div');
+                bg.classList.add('dark-block');
+                body.append(bg);
+                let item = document.createElement('div');
+                item.classList.add('item-block');
+                item.innerHTML = `
+                    <img src='${data[i].urls.regular}' class="item-block-img" data-num="${data[i].id}"></img>
+                    <div class="like-container">
+                        <img src='${imgSrc}' class="like"></img>
+                        <p class="like-count">${numForLikes}</p>
+                    </div>
+                    `
             
-            body.append(item);
-            let itemBlockImg = document.querySelector('.item-block-img');
-            itemBlockImg.onload = function(){
+                body.append(item);
+                let itemBlockImg = document.querySelector('.item-block-img');
+                itemBlockImg.onload = function(){
                 let itemBlock = document.querySelector('.item-block');
-            itemBlock.style.left = ((window.innerWidth - itemBlockImg.offsetWidth) / 2) + 'px';
-            console.log(window.screen.width);
-            console.log(itemBlockImg.clientWidth);
-        let like = document.querySelector('.like');
-        like.addEventListener('click', () => {
+                itemBlock.style.left = (((window.innerWidth - itemBlockImg.offsetWidth) / 2) + 'px') || ((40 / 2) + '%');
+                let like = document.querySelector('.like');
+                like.addEventListener('click', () => {
+
+                    let imgNum = this.offsetParent.children[0].dataset.num;
+                    let likeCount = document.querySelector('.like-count');
+                        if(likedPhoto.has(imgNum)){
+                            numOfImg = likeCount.innerHTML = +likeCount.innerHTML - 1;
+                            like.setAttribute("src", "./img/heart.png");
+                            likedPhoto.delete(imgNum);
+                            let gallery = document.querySelectorAll('.gallery');
+
+                            if(gallery.length !=0 && isLike == true){
+
+                                containerForFavImg.style.display = 'none';
+                                containerForFavImg.innerHTML = '';
+                                favImg.click();
+                            }
+                            if(gallery.length != 0){
+                                containerForFavImg.innerHTML = '';
+                            addImgForFavPage();
+                    
+                            }
+                        } else if(!likedPhoto.has(imgNum)) {
+                            numOfImg = likeCount.innerHTML = +likeCount.innerHTML + 1;
+                            like.setAttribute('src', './img/heard-fulled.png');
+                            likedPhoto.add(imgNum);
+                        }
+                })
+                }
             
-            console.dir(this)
-        console.log(like)
-        // like.addEventListener("click", (e) => {
-
-        // let target = e.target;
-        
-        // let likedPhoto = new Set();
-        // let isLike = false;
-        
-
-        
-        
-        console.dir(target)
-        console.log(target.dataset.num)
-        // console.log(imgNum)
-        console.log(this)
-        // console.log(target.className == 'like' && isLike && likedPhoto.has(imgNum))
-        // like.addEventListener("click", () => {
-            // if(target.className == 'like'){
-                
-                let imgNum = this.offsetParent.children[0].dataset.num;
-                let likeCount = document.querySelector('.like-count');
-                if(likedPhoto.has(imgNum)){
-                    console.log('here')
-                // isLike = false;
-                numOfImg = likeCount.innerHTML = +likeCount.innerHTML - 1;
-                like.setAttribute("src", "./img/heart.png");
-                likedPhoto.delete(imgNum);
-                console.log(likedPhoto)
-                // return;
-                // let index = likedPhoto.findIndex(photo => photo == target.id);
-                // likedPhoto.splice(index, 1);
-            } else if(!likedPhoto.has(imgNum)) {
-                numOfImg = likeCount.innerHTML = +likeCount.innerHTML + 1;
-            like.setAttribute('src', './img/heard-fulled.png');
-            likedPhoto.add(imgNum);
-            console.log(likedPhoto);
-            // return;
+                document.body.style.overflow = 'hidden';
             }
-        
-        })
 
-
-                // isLike = true;
-            
-            
-            // }
-
-            
-            
-            // if(isLike && likedPhoto.includes(target.id)){
-            //     like.setAttribute("src", "./img/heart.png");
-            //     // likedPhoto.filter((photo) => photo != target.id);
-            //     let index = likedPhoto.findIndex(photo => photo == target.id);
-            //     likedPhoto.splice(index, 1);
-            // }
-            // console.log(target.this)
-        // })
-
-        // function checkLike (){
-        //     if(target.className == 'like' && isLike && likedPhoto.has(target.id)){
-        //         isLike = false;
-        //         like.setAttribute("src", "./img/heart.png");
-        //         likedPhoto.delete(target.id);
-        //         console.log(likedPhoto)
-        //         return
-        //         // let index = likedPhoto.findIndex(photo => photo == target.id);
-        //         // likedPhoto.splice(index, 1);
-        //     }
-        // }
-
-        // like.addEventListener('click', checkLike);
-    // })
-
-    
-            }
-            
-
-
-            document.body.style.overflow = 'hidden';
         }
 
-        
-
-        // let like = document.querySelector('.like');
-
-        // like.addEventListener("like", () => {
-        //     like.setAttribute('src', './img/heard-fulled.png');
-        //     likedPhoto.push(target.id);
-
-        //     if(likedPhoto.includes(target.id)){
-        //         like.setAttribute("src", "./img/heart.png");
-        //         likedPhoto.filter((photo) => photo != target.id)
-        //     }
-        //     console.log(likedPhoto)
-        // })
-
-        }
-        console.log(e.target.classList.contains("like"))
-// if(e.target.classList.contains("like")){
-//     let like = document.querySelector('.like');
-// console.log(like)
-//         // like.addEventListener("click", (e) => {
-
-//         // let target = e.target;
-        
-//         // let likedPhoto = new Set();
-//         // let isLike = false;
-        
-
-        
-        
-//         console.dir(target)
-//         console.log(target.dataset.num)
-//         // console.log(imgNum)
-//         console.log(this)
-//         // console.log(target.className == 'like' && isLike && likedPhoto.has(imgNum))
-//         // like.addEventListener("click", () => {
-//             // if(target.className == 'like'){
-                
-//                 let imgNum = target.offsetParent.offsetParent.firstElementChild.dataset.num;
-//                 let likeCount = document.querySelector('.like-count');
-//                 if(likedPhoto.has(imgNum)){
-//                     console.log('here')
-//                 // isLike = false;
-//                 numOfImg = likeCount.innerHTML = +likeCount.innerHTML - 1;
-//                 like.setAttribute("src", "./img/heart.png");
-//                 likedPhoto.delete(imgNum);
-//                 console.log(likedPhoto)
-//                 // return;
-//                 // let index = likedPhoto.findIndex(photo => photo == target.id);
-//                 // likedPhoto.splice(index, 1);
-//             } else if(!likedPhoto.has(imgNum)) {
-//                 numOfImg = likeCount.innerHTML = +likeCount.innerHTML + 1;
-//             like.setAttribute('src', './img/heard-fulled.png');
-//             likedPhoto.add(imgNum);
-//             console.log(likedPhoto);
-//             // return;
-//             }
-//                 // isLike = true;
-            
-            
-//             // }
-
-            
-            
-//             // if(isLike && likedPhoto.includes(target.id)){
-//             //     like.setAttribute("src", "./img/heart.png");
-//             //     // likedPhoto.filter((photo) => photo != target.id);
-//             //     let index = likedPhoto.findIndex(photo => photo == target.id);
-//             //     likedPhoto.splice(index, 1);
-//             // }
-//             // console.log(target.this)
-//         // })
-
-//         // function checkLike (){
-//         //     if(target.className == 'like' && isLike && likedPhoto.has(target.id)){
-//         //         isLike = false;
-//         //         like.setAttribute("src", "./img/heart.png");
-//         //         likedPhoto.delete(target.id);
-//         //         console.log(likedPhoto)
-//         //         return
-//         //         // let index = likedPhoto.findIndex(photo => photo == target.id);
-//         //         // likedPhoto.splice(index, 1);
-//         //     }
-//         // }
-
-//         // like.addEventListener('click', checkLike);
-//     // })
-// }
-        
-        
         if(target.className == 'dark-block'){
+            
             let itemBlock = document.querySelector('.item-block');
             let darkBlock = document.querySelector('.dark-block');
-            document.body.style.overflow = '';
-            itemBlock.remove();
-            darkBlock.remove();
+            if(itemBlock && darkBlock){
+                itemBlock.remove();
+                darkBlock.remove();
+            
+                document.body.style.overflow = '';
+            }
+            
         }
-
-//         if(target.className == 'fav-img'){
-
-//                 main.innerHTML = '';
-//             console.log(likedPhoto, data)
-//             // data.map((data) => {
-//             for(let value of likedPhoto){
-//                 for(let j=0; j<data.length; j++){
-//                     // console.log(value)
-//                     // console.log(data[j].id)
-//                     if(value == data[j].id){
-//                         console.log(data[j].urls.small)
-// let imgFav = document.createElement("img");
-//         imgFav.classList.add("gallery");
-//         // imgFav.classList.add("fav-img-checked");
-//         imgFav.setAttribute('id', data[j].id)
-//         imgFav.src = data[j].urls.small;
-//         imgFav.alt = data[j].alt_description;
-//         main.append(imgFav); 
-//                 }
-                
-//                 }
-//             }
-            
-        
-            
-        
-
-//         // setInterval(() => check(), 500)
-        
-//     // })
-//         }
     })
 
+    function addImgForFavPage () {
 
+        container.style.display = 'none';
+        containerForFavImg.style.display = 'flex';
 
-    // main.innerHTML = '';
-// let like = document.querySelector('.like');
-// console.log(like)
-//         like.addEventListener("click", (e) => {
-
-//         let target = e.target;
-        
-//         // let likedPhoto = new Set();
-//         // let isLike = false;
-        
-
-        
-        
-//         console.dir(target)
-//         console.log(target.dataset.num)
-//         // console.log(imgNum)
-//         console.log(likedPhoto.has(target.id))
-//         // console.log(target.className == 'like' && isLike && likedPhoto.has(imgNum))
-//         // like.addEventListener("click", () => {
-//             // if(target.className == 'like'){
-                
-//                 let imgNum = target.offsetParent.offsetParent.firstElementChild.dataset.num;
-//                 let likeCount = document.querySelector('.like-count');
-//                 if(likedPhoto.has(imgNum)){
-//                     console.log('here')
-//                 // isLike = false;
-//                 numOfImg = likeCount.innerHTML = +likeCount.innerHTML - 1;
-//                 like.setAttribute("src", "./img/heart.png");
-//                 likedPhoto.delete(imgNum);
-//                 console.log(likedPhoto)
-//                 // return;
-//                 // let index = likedPhoto.findIndex(photo => photo == target.id);
-//                 // likedPhoto.splice(index, 1);
-//             } else if(!likedPhoto.has(imgNum)) {
-//                 numOfImg = likeCount.innerHTML = +likeCount.innerHTML + 1;
-//             like.setAttribute('src', './img/heard-fulled.png');
-//             likedPhoto.add(imgNum);
-//             console.log(likedPhoto);
-//             // return;
-//             }
-//                 // isLike = true;
-            
-            
-//             // }
-
-            
-            
-//             // if(isLike && likedPhoto.includes(target.id)){
-//             //     like.setAttribute("src", "./img/heart.png");
-//             //     // likedPhoto.filter((photo) => photo != target.id);
-//             //     let index = likedPhoto.findIndex(photo => photo == target.id);
-//             //     likedPhoto.splice(index, 1);
-//             // }
-//             // console.log(target.this)
-//         // })
-
-//         // function checkLike (){
-//         //     if(target.className == 'like' && isLike && likedPhoto.has(target.id)){
-//         //         isLike = false;
-//         //         like.setAttribute("src", "./img/heart.png");
-//         //         likedPhoto.delete(target.id);
-//         //         console.log(likedPhoto)
-//         //         return
-//         //         // let index = likedPhoto.findIndex(photo => photo == target.id);
-//         //         // likedPhoto.splice(index, 1);
-//         //     }
-//         // }
-
-//         // like.addEventListener('click', checkLike);
-//     })
-
-    favImg.addEventListener('click', () => {
-
-                container.style.display = 'none';
-                containerForFavImg.style.display = 'flex';
-                // let containerForFavImg = document.createElement('div');
-                // containerForFavImg.classList.add('container-for-fav-img');
-                // container.after(containerForFavImg); 
-            
-            // data.map((data) => {
             for(let value of likedPhoto){
-                
+
                 for(let j=0; j<data.length; j++){
                     
-                    // console.log(value)
-                    // console.log(data[j].id)
                     if(value == data[j].id){
-                        console.log(data[j].urls.small)
-                        console.log(likedPhoto)
-let imgFav = document.createElement("img");
-        imgFav.classList.add("gallery");
-        // imgFav.classList.add("fav-img-checked");
-        imgFav.setAttribute('id', data[j].id)
-        imgFav.src = data[j].urls.small;
-        imgFav.alt = data[j].alt_description;
-        containerForFavImg.append(imgFav); 
-                }
+                        let imgFav = document.createElement("img");
+                        imgFav.classList.add("gallery");
+                        imgFav.setAttribute('id', data[j].id)
+                        imgFav.src = data[j].urls.small;
+                        imgFav.alt = data[j].alt_description;
+                        containerForFavImg.append(imgFav); 
+                    }
                 
                 }
+            }
+
+
+            if(likedPhoto.size == 0){   
+                
+                empty.style.display = "block";
             }
 
             if(container.style.display == 'none'){
                 favImg.style.pointerEvents = 'none';
             }
-            
-        
-            
-        
+        }
 
-        // setInterval(() => check(), 500)
-        
-    // })
-        })
+        favImg.addEventListener('click', addImgForFavPage);
 }
-
-
 
     let searchBtn = document.querySelector('button');
 
@@ -422,21 +165,23 @@ let imgFav = document.createElement("img");
         if (searchField.value != '') {
             url = `https://api.unsplash.com/photos/random?orientation=landscape&client_id=4arZoJEL1KuBR9gU7Mtfno9am5Ct7rzvjET3Jc3l9lQ&count=20&query=${searchField.value}`;
             main.innerHTML = '';
+            isLike = true;
             favImg.style.pointerEvents = 'auto';
             if(container.style.display == 'none'){
                 container.style.display = 'flex';
                 containerForFavImg.style.display = 'none';
+                containerForFavImg.innerHTML = '';
             }
             let gallery = document.querySelectorAll('.gallery');
             if(gallery.length != 0){
-                gallery.forEach((item) => {
-                    item.remove();
-                })
+                containerForFavImg.innerHTML = '';
+                
+                if(likedPhoto.size == 0){   
+                
+                    empty.style.display = "block";
+                }
             }
-            // console.log(searchField.value)
-            // searchField.value = searchField.value;
             getData();
-            // getData();
         } else {
             alert("Please, enter the word");
         }
